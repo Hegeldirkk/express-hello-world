e3bb5a287edba19e687720eb3c58a8826b7de06e
Diabakate Ikary <hegeldirkk@gmail.com>
2023-03-26 18:20:09 +0000

    correct

--- app.js
+++ app.js
@@ -1,43 +1,92 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-// #############################################################################
-// Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
+// express app
+
+const app = express();
+
+// Connect to MongoDB..... uncomment to connect to the Database
+const dbURI = 'mongodb+srv://swift:Magnetic123@alx-project.fkxjfut.mongodb.net/alx-portfolio-project?retryWrites=true&w=majority'
+
+
+mongoose.connect(dbURI)
+    .then( () => app.listen(3000))
+    
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+    
 });
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
+app.get('/about', (req, res) => {
+    // res.send('<p>Home Page</p>')
+    res.render('about', { title: 'About Page'});
+});
 
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort( { createdAt: -1})
+      .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result})
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+})
+app.get('/blogs/create', (req, res) => {
+    // res.send('<p>Home Page</p>')
+    res.render('create', { title: 'Create a New Blog'});
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+      .then((result) => {
+        res.redirect('/blogs');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
 })
 
-module.exports = app
+app.get('/blogs/:id', (req, res) =>{
+    const id = req.params.id;
+    Blog.findById(id)
+      .then((result) => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch((err) => {
+        res.status(404).render('404', { title: 'Blog not found'});
+      });
+     
+       
+})
+
+
+app.delete('/blogs/:id', (req, res) =>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+      .then((result) => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+     
+       
+})
+
+app.use((req, res) => {
+    // res.send('<p>Home Page</p>')
+    res.status(404).render('404', { title: '404'});
+});
